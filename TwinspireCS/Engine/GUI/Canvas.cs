5b983f6804fa1c82829a8e7b45593bf6a3b7ecb4
tienery <luke@lukeselman.com>
2023-04-06 16:45:45 +0100

    Add Canvas.

---
+++ TwinspireCS/Engine/GUI/Canvas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinspireCS.Engine.GUI
+{
+    public class Canvas
+    {
+
+        private List<Grid> layouts;
+        private List<int> elements;
+        private List<object> elementContent;
+
+        public Canvas()
+        {
+            layouts = new List<Grid>();
+            elements = new List<int>();
+            elementContent = new List<object>();
+        }
+
+
+    }
+}
