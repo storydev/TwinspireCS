f5b5d1ed2a6d71c8faf16bef5855e2e8a418ebce
tienery <luke@lukeselman.com>
2023-04-06 10:55:16 +0100

    Add Grid for UI.

---
+++ TwinspireCS/Engine/GUI/Grid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinspireCS.Engine.GUI
+{
+    public class Grid
+    {
+
+        public Vector4 Dimension;
+        public float[] Columns;
+        public float[] Rows;
+        public float[] Margin;
+        public float[] Padding;
+
+        public Grid()
+        {
+            Dimension = new Vector4();
+            Columns = Array.Empty<float>();
+            Rows = Array.Empty<float>();
+            Margin = Array.Empty<float>();
+            Padding = Array.Empty<float>();
+        }
+
+    }
+}
