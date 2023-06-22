1c561dcaaea7ef294fcfe2fe22c26fdf8ce9a29b
tienery <luke@lukeselman.com>
2023-06-22 19:55:32 +0100

    Finalise tile map options.

--- TwinspireCS/Engine/Graphics/TileMap.cs
+++ TwinspireCS/Engine/Graphics/TileMap.cs
@@ -9,7 +9,20 @@ namespace TwinspireCS.Engine.Graphics
     public class TileMap
     {
 
+        public string Name;
+        public List<TileLayer> Layers;
+        public int TileSize;
+        public int TilesX;
+        public int TilesY;
 
+        public TileMap()
+        {
+            Name = string.Empty;
+            Layers = new List<TileLayer>();
+            TileSize = 48;
+            TilesX = 25;
+            TilesY = 25;
+        }
 
     }
 }
