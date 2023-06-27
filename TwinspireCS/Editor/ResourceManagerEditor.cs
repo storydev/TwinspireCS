8bc4d605564327bda6c2d482a41536a4422eb4e3
tienery <luke@lukeselman.com>
2023-06-27 21:20:36 +0100

    Add some navigation options.

--- TwinspireCS/Editor/ResourceManagerEditor.cs
+++ TwinspireCS/Editor/ResourceManagerEditor.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Numerics;
 
 namespace TwinspireCS.Editor
@@ -16,6 +17,14 @@ namespace TwinspireCS.Editor
         static int page;
         static List<ResourceFile> resources;
         static string[] tableCells;
+        static string[] packageNames;
+        static int selectedPackage;
+        static int lastSelectedPackage;
+        static bool allowBack;
+        static bool allowForward;
+
+
+        static string resourcePackageName;
 
         public static void Init()
         {
@@ -23,6 +32,7 @@ namespace TwinspireCS.Editor
             page = 0;
             resources = new List<ResourceFile>();
             tableCells = Array.Empty<string>();
+            resourcePackageName = string.Empty;
 
             var rm = Application.Instance.ResourceManager;
             for (int i = 0; i < rm.Packages.Count(); i++)
@@ -39,6 +49,7 @@ namespace TwinspireCS.Editor
                 }
             }
 
+            ResetPackageNames();
             ResetCellData();
         }
 
@@ -48,25 +59,129 @@ namespace TwinspireCS.Editor
 
             if (ImGui.Begin("Resource Manager", ref isOpen))
             {
-                ImGui.BeginTable("ResourceManagerFiles", 6);
-                ImGui.TableSetupColumn("Identifier", ImGuiTableColumnFlags.None, 250f);
-                ImGui.TableSetupColumn("Package", ImGuiTableColumnFlags.None, 150f);
-                ImGui.TableSetupColumn("Original Source File", ImGuiTableColumnFlags.DefaultHide, 300f);
-                ImGui.TableSetupColumn("File Ext", ImGuiTableColumnFlags.None, 90f);
-                ImGui.TableSetupColumn("Size", ImGuiTableColumnFlags.None, 125f);
-                ImGui.TableSetupColumn("Compressed Size", ImGuiTableColumnFlags.None, 125f);
-                ImGui.TableSetupScrollFreeze(0, 1);
-                ImGui.TableHeadersRow();
-
-                for (int i = 0; i < tableCells.Length; i++)
+                ImGui.Text("Package:"); ImGui.SameLine();
+                ImGui.SetNextItemWidth(200f);
+                ImGui.Combo("##PackageList", ref selectedPackage, packageNames, packageNames.Length); ImGui.SameLine();
+                if (lastSelectedPackage != selectedPackage)
                 {
-                    ImGui.TableNextColumn();
-                    ImGui.Text(tableCells[i]);
+                    ResetCellData();
+                    lastSelectedPackage = selectedPackage;
                 }
 
-                ImGui.EndTable();
+                if (ImGui.Button("+##AddResourcePackage"))
+                {
+                    ImGui.OpenPopup("AddResourcePackagePopup");
+                }
+
+                if (ImGui.BeginPopup("AddResourcePackagePopup"))
+                {
+                    ImGui.Text("Name:"); ImGui.SameLine();
+                    ImGui.InputText("##Name", ref resourcePackageName, 64);
+
+
+                    if (ImGui.Button("Confirm##ConfirmAddResourcePackage"))
+                    {
+                        if (!resourcePackageName.EndsWith(".dat"))
+                            resourcePackageName += ".dat";
+
+                        var validPath = Utils.ValidateFilePath(Path.Combine(Application.Instance.ResourceManager.AssetDirectory, resourcePackageName), true, true);
+                        if (validPath && !File.Exists(resourcePackageName))
+                        {
+                            Application.Instance.ResourceManager.CreatePackage(resourcePackageName);
+                            selectedPackage = 0;
+                            page = 0;
+                            ResetPackageNames();
+                            ImGui.CloseCurrentPopup();
+                        }
+                    }
+
+                    ImGui.EndPopup();
+                }
+
+                ImGui.SameLine();
+
+                if (!allowBack)
+                    ImGui.BeginDisabled();
+
+                if (ImGui.Button("<##PreviousRMPage"))
+                {
+                    page -= 1;
+                    ResetCellData();
+                }
+
+                if (!allowBack)
+                    ImGui.EndDisabled();
+
+                ImGui.SameLine();
+
+                if (!allowForward)
+                    ImGui.BeginDisabled();
+
+                if (ImGui.Button(">##NextRMPage"))
+                {
+                    page += 1;
+                    ResetCellData();
+                }
+
+                if (!allowForward)
+                    ImGui.EndDisabled();
+
+
+                ImGui.Separator();
+
+                if (ImGui.BeginTabBar("ResourceResults"))
+                {
+                    if (ImGui.BeginTabItem("Resources"))
+                    {
+                        ImGui.BeginTable("ResourceManagerFiles", 6);
+                        ImGui.TableSetupColumn("Identifier", ImGuiTableColumnFlags.None, 250f);
+                        ImGui.TableSetupColumn("Package", ImGuiTableColumnFlags.None, 150f);
+                        ImGui.TableSetupColumn("Original Source File", ImGuiTableColumnFlags.DefaultHide, 300f);
+                        ImGui.TableSetupColumn("File Ext", ImGuiTableColumnFlags.None, 90f);
+                        ImGui.TableSetupColumn("Size", ImGuiTableColumnFlags.None, 125f);
+                        ImGui.TableSetupColumn("Compressed Size", ImGuiTableColumnFlags.None, 125f);
+                        ImGui.TableSetupScrollFreeze(0, 1);
+                        ImGui.TableHeadersRow();
+
+                        for (int i = 0; i < tableCells.Length; i++)
+                        {
+                            ImGui.TableNextColumn();
+                            ImGui.Text(tableCells[i]);
+                        }
+
+                        ImGui.EndTable();
+
+                        ImGui.EndTabItem();
+                    }
+
+                    if (ImGui.BeginTabItem("Add Resources"))
+                    {
+
+
+                        ImGui.EndTabItem();
+                    }
+
+                    ImGui.EndTabBar();
+                }
 
                 ImGui.End();
+            }
+        }
+
+        static void ResetPackageNames()
+        {
+            var rm = Application.Instance.ResourceManager;
+            packageNames = new string[rm.Packages.Count() + 1];
+            for (int i = 0; i < packageNames.Length; i++)
+            {
+                if (i == 0)
+                {
+                    packageNames[i] = "All";
+                }
+                else
+                {
+                    packageNames[i] = rm.Packages.ElementAt(i - 1).SourceFilePath;
+                }
             }
         }
 
@@ -76,7 +191,9 @@ namespace TwinspireCS.Editor
             var max = min + maxItems;
             int total = resources.Count - 1;
 
-            if (resources.Count > max)
+            allowBack = min > 0;
+            allowForward = resources.Count > max;
+            if (allowForward)
                 total = max;
 
             var totalCells = 6 * total;
@@ -85,12 +202,24 @@ namespace TwinspireCS.Editor
             var rm = Application.Instance.ResourceManager;
             
             var cellIndex = 0;
-            for (int i = min; min < max; i++)
+            int index = min;
+            while (index < max)
             {
-                if (i >= resources.Count - 1)
+                if (index >= resources.Count - 1)
                     break;
 
-                var resource = resources[i];
+                var resource = resources[index];
+                var filteredPackage = -1;
+                if (selectedPackage > 0)
+                {
+                    filteredPackage = selectedPackage - 1;
+                }
+
+                if (filteredPackage > -1 && resource.PackageIndex != filteredPackage)
+                {
+                    continue;
+                }
+
                 var package = rm.Packages.ElementAt(resource.PackageIndex);
                 tableCells[cellIndex++] = resource.Name;
                 tableCells[cellIndex++] = package.SourceFilePath;
@@ -98,7 +227,11 @@ namespace TwinspireCS.Editor
                 tableCells[cellIndex++] = resource.Segment.FileExt;
                 tableCells[cellIndex++] = ConvertBytesToString(resource.Segment.Size);
                 tableCells[cellIndex++] = ConvertBytesToString(resource.Segment.CompressedSize);
+
+                index += 1;
             }
+
+
         }
 
         static string ConvertBytesToString(long bytes)
--- TwinspireCS/Utils.cs
+++ TwinspireCS/Utils.cs
@@ -11,6 +11,54 @@ namespace TwinspireCS
     internal unsafe class Utils
     {
 
+
+        /// <summary>
+        /// Function taken from: https://stackoverflow.com/a/26062198
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="IncludeFileName"></param>
+        /// <param name="RequireFileName"></param>
+        /// <returns></returns>
+        public static bool ValidateFilePath(string path, bool IncludeFileName, bool RequireFileName = false)
+        {
+            if (string.IsNullOrEmpty(path)) { return false; }
+            string root = null;
+            string directory = null;
+            string filename = null;
+            try
+            {
+                // throw ArgumentException - The path parameter contains invalid characters, is empty, or contains only white spaces.
+                root = Path.GetPathRoot(path);
+
+                // throw ArgumentException - path contains one or more of the invalid characters defined in GetInvalidPathChars.
+                // -or- String.Empty was passed to path.
+                directory = Path.GetDirectoryName(path);
+
+                // path contains one or more of the invalid characters defined in GetInvalidPathChars
+                if (IncludeFileName) { filename = Path.GetFileName(path); }
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            // null if path is null, or an empty string if path does not contain root directory information
+            if (string.IsNullOrEmpty(root)) { return false; }
+
+            // null if path denotes a root directory or is null. Returns String.Empty if path does not contain directory information
+            if (string.IsNullOrEmpty(directory)) { return false; }
+
+            if (RequireFileName)
+            {
+                // if the last character of path is a directory or volume separator character, this method returns String.Empty
+                if (string.IsNullOrEmpty(filename)) { return false; }
+
+                // check for illegal chars in filename
+                if (filename.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0) { return false; }
+            }
+            return true;
+        }
+
         public static sbyte* GetSByteFromString(string value)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(value);
