71bf8be7b9f522c546b41668ece3cde9cb1d1f1f
tienery <luke@lukeselman.com>
2023-08-10 19:07:12 +0100

    Complete type definitions for Tiled.

--- TwinspireCS.Tiled/Json/TiledLayer.cs
+++ TwinspireCS.Tiled/Json/TiledLayer.cs
@@ -8,5 +8,42 @@ namespace TwinspireCS.Tiled.Json
 {
     public class TiledLayer
     {
+
+        public TiledChunk[]? chunks;
+        public string? @class;
+        public string compression;
+        public object data;
+        public string draworder;
+        public string encoding;
+        public int height;
+        public int id;
+        public string image;
+        public TiledLayer[] layers;
+        public bool locked;
+        public string name;
+        public TiledObject[] objects;
+        public double offsetx;
+        public double offsety;
+        public double opacity;
+        public double parallaxx;
+        public double parallaxy;
+        public TiledProperty[] properties;
+        public bool repeatx;
+        public bool repeaty;
+        public int startx;
+        public int starty;
+        public string tintcolor;
+        public string transparentcolor;
+        public string type;
+        public bool visible;
+        public int width;
+        public int x;
+        public int y;
+
+        public TiledLayer()
+        {
+
+        }
+
     }
 }
--- TwinspireCS.Tiled/Json/TiledMap.cs
+++ TwinspireCS.Tiled/Json/TiledMap.cs
@@ -8,5 +8,35 @@ namespace TwinspireCS.Tiled.Json
 {
     public class TiledMap
     {
+
+        public string backgroundcolor;
+        public string @class;
+        public int compressionlevel;
+        public int height;
+        public int hexsidelength;
+        public bool infinite;
+        public TiledLayer[] layers;
+        public int nextlayerid;
+        public int nextobjectid;
+        public string orientation;
+        public double parallaxoriginx;
+        public double parallaxoriginy;
+        public TiledProperty[] properties;
+        public string renderorder;
+        public string staggeraxis;
+        public string staggerindex;
+        public string tiledversion;
+        public int tileheight;
+        public TiledTileset tilesets;
+        public int tilewidth;
+        public string type;
+        public string version;
+        public int width;
+
+        public TiledMap()
+        {
+
+        }
+
     }
 }
